a1391abef29152c382905e57999f574a860a6430
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-25 22:56:58 +0000

    upload

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,7 +2,8 @@ import type { JSX } from "react";
 import useDarkMode from "../hooks/useDarkMode";
 import type { Project } from "../type";
 
-const movieGptVideoUrl = "/moviegpt.mp4";
+const movieGptVideoUrl =
+  "https://www.dropbox.com/scl/fi/isi8cmsa1gz9bt9glhtpf/Movie-GPT.mp4?rlkey=phz7ukvucfymrmsadnqjc0snq&st=xx29objo&dl=1";
 
 const projects: Project[] = [
   {
